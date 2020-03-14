bb9a8b70edc51492302a4cc3325fc52e43270372
llgx10 <42101950+llgx10@users.noreply.github.com>
2020-03-14 22:06:01 +0700

    Add files via upload

---
+++ Chap 1-Algorithm Flowchart/1+2+3+4+n/program/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int S=0, n;
+         cout <<" nhap so duong n: ";
+        cin>>n;
+    for (int i=0; i<=n; i++)
+        {
+            S+=i;
+         }
+    cout <<"\nS= "<< S;
+    return 0;
+}
