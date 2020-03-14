1de3f4469efbf558c354c76a1a43039e72e7560b
llgx10 <42101950+llgx10@users.noreply.github.com>
2020-03-15 04:26:34 +0700

    Update main.cpp

--- Chap 1-Algorithm Flowchart/1+2+3+4+n/program/main.cpp
+++ Chap 1-Algorithm Flowchart/1+2+3+4+n/program/main.cpp
@@ -5,8 +5,12 @@ using namespace std;
 int main()
 {
     int S=0, n;
-         cout <<" nhap so duong n: ";
-        cin>>n;
+         do
+        {
+            cout <<" nhap so duong n: ";
+            cin>>n;
+         }
+         while (n<=0);
     for (int i=0; i<=n; i++)
         {
             S+=i;
