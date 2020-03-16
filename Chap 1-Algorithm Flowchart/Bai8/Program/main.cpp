b6a9147cce2ad48a0396cfeecded94fdef752e3f
LLong <42101950+llgx10@users.noreply.github.com>
2020-03-16 19:35:05 +0700

    Add files via upload

---
+++ Chap 1-Algorithm Flowchart/Bai8/Program/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    float s=0;
+    do
+    {
+        cout<<"nhap so duong n: ";
+        cin>>n;
+    }
+    while(n<=0);
+        for (int i=1;i<=n;i++)
+    {
+        s=s+((2*i+1.0)/(2*i+2.0));
+    }
+    cout<<"\ns = "<<s;
+    return 0;
+}
